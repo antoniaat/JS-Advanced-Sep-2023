e8b440fc3af92cfac2f4ba1c09f4e7b1863b5dbf
Nikolay Stoyanov <45856056+NStoyanov95@users.noreply.github.com>
2023-09-21 12:15:05 +0300

    Update 06.storeCatalogue.js

--- 03-objects-and-composition/exercise/06.storeCatalogue.js
+++ 03-objects-and-composition/exercise/06.storeCatalogue.js
@@ -14,11 +14,3 @@ function solve(arr) {
         console.log(`  ${product}: ${price}`);
     });
 };
-solve(['Appricot : 20.4',
-    'Fridge : 1500',
-    'TV : 1499',
-    'Deodorant : 10',
-    'Boiler : 300',
-    'Apple : 1.25',
-    'Anti-Bug Spray : 15',
-    'T-Shirt : 10'])
