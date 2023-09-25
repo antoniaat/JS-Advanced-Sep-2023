1417b48015818ca4a35931e4003b34a4a1182741
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2023-09-25 08:25:58 +0100

    Update 09-numberConvertor.js
    I made the changes and the function that converts is  changed. I used toString(16) to convert the number and toUpperCase() to make the letters uppercase

--- 04-dom/exercise/09-numberConvertor.js
+++ 04-dom/exercise/09-numberConvertor.js
@@ -1,11 +1,12 @@
 function solve() {
 
-    let optionBinary = document.createElement("OPTION");
-    let optionHexadecimal = document.createElement("OPTION");
+    const optionBinary = document.createElement("OPTION");
+    const optionHexadecimal = document.createElement("OPTION");
 
     const input = document.getElementById('input');
     const selectMenuTo = document.getElementById('selectMenuTo');
     const btn = document.getElementsByTagName('button');
+    const convertButton = btn[0]
     const result = document.getElementById('result');
 
     optionBinary.textContent = 'Binary';
@@ -15,7 +16,7 @@ function solve() {
     selectMenuTo.appendChild(optionBinary);
     selectMenuTo.appendChild(optionHexadecimal);
 
-    btn[0].onclick = function () {
+    convertButton.onclick = function () {
         if (selectMenuTo.value == 'binary') {
             result.value = convertToBinary(Number(input.value));
         } else if (selectMenuTo.value == 'hexadecimal') {
@@ -34,26 +35,7 @@ function solve() {
     }
 
     function convertToHeXadecimal(num) {
-        let result = [];
-        while (num > 0) {
-            let digit = num % 16;
-            if (digit < 10) {
-                result.unshift(digit)
-            } else if (digit == 10) {
-                result.unshift('A');
-            } else if (digit == 11) {
-                result.unshift('B');
-            } else if (digit == 12) {
-                result.unshift('C');
-            } else if (digit == 13) {
-                result.unshift('D');
-            } else if (digit == 14) {
-                result.unshift('E');
-            } else if (digit == 15) {
-                result.unshift('F');
-            }
-            num = Math.floor(num / 16);
-        }
-        return result.join('');
+        let result = num.toString(16).toUpperCase()
+        return result
     }
 }
