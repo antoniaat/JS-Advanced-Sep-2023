cc23ecf18ca087377aebaa56b30083e0b7173d0c
Zara <133760099+zlatozaraZlatkova@users.noreply.github.com>
2023-09-26 09:46:19 +0200

    Update solution.js
    Replacing let with const

--- 04-dom/exercise/solution.js
+++ 04-dom/exercise/solution.js
@@ -1,16 +1,16 @@
 function solve() {
-  let text = document.getElementById("text").value;
-  let namingConvention = document.getElementById("naming-convention").value;
-  let result = convertText(text, namingConvention);
+  const text = document.getElementById("text").value;
+  const namingConvention = document.getElementById("naming-convention").value;
+  const result = convertText(text, namingConvention);
 
   document.getElementById("result").textContent = result;
 
   function convertText(text, convention) {
-    let words = text.toLowerCase().split(" ");
+    const words = text.toLowerCase().split(" ");
     if (convention !== "Camel Case" && convention !== "Pascal Case") {
       return "Error!";
     }
-
+    
     for (let i = 0; i < words.length; i++) {
       if (i !== 0 || convention === "Pascal Case") {
         words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
