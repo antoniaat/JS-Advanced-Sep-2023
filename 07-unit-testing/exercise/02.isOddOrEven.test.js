e11751dff397d3f70d707e0ed164ab1a8d7ab5c4
BayryamB <129975813+BayryamB@users.noreply.github.com>
2023-10-05 21:18:03 +0300

    02.isOddOrEven.test.js
    change "must return" to "should return"

--- 07-unit-testing/exercise/02.isOddOrEven.test.js
+++ 07-unit-testing/exercise/02.isOddOrEven.test.js
@@ -1,13 +1,13 @@
 const {assert} = require('chai');
 const {isOddOrEven} = require('./isOddOrEven.js');
 describe("test with correct params", () => {
-    it("must return ODD ", () => {
+    it("should return ODD ", () => {
         assert.equal(isOddOrEven("123"), "odd");
     })
-    it("must return EVEN ", () => {
+    it("should return EVEN ", () => {
         assert.equal(isOddOrEven("4356"), "even");
     })
-    it("must return undefined ", () => {
+    it("should return undefined ", () => {
         assert.equal(isOddOrEven([1]), undefined);
     })
 })
