9010c3559ee131c10f34cd114f1e864320a27fd3
BayryamB <129975813+BayryamB@users.noreply.github.com>
2023-10-05 19:52:22 +0300

    02.isOddOrEven.js

---
+++ 07-unit-testing/exercise/02.isOddOrEven.js
@@ -0,0 +1,13 @@
+function isOddOrEven(string) {
+    if (typeof(string) !== 'string') {
+        return undefined;
+    }
+    if (string.length % 2 === 0) {
+        return "even";
+    }
+
+    return "odd";
+}
+module.exports = {
+    isOddOrEven
+}
