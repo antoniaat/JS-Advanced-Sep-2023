3bd760d669f34db9e8415e97815229c3d2988fa4
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2023-10-05 11:50:54 +0100

    Update solution.js
    Changet to const variables and better syntaxis for the funcObject

--- 05-dom-events/exercise/02. Time-Converter/solution.js
+++ 05-dom-events/exercise/02. Time-Converter/solution.js
@@ -1,9 +1,8 @@
 function attachEventsListeners() {
-  function attachEventsListeners() {
-    let main = document.querySelector("main");
+  const main = document.querySelector("main");
 
     main.addEventListener("click", convert);
-    let inputsCollection = document.querySelectorAll('main input[type="text"]');
+    const inputsCollection = document.querySelectorAll('main input[type="text"]');
 
     function convert(element) {
       if (element.target.value == "Convert") {
@@ -12,11 +11,11 @@ function attachEventsListeners() {
     }
 
     function convertAll(button) {
-      let div = button.parentElement;
-      let input = div.querySelector('input[type="text"]');
-      let label = input.id;
-      let num = Number(input.value);
-      let resultObj = calc(label, num);
+      const div = button.parentElement;
+      const input = div.querySelector('input[type="text"]');
+      const label = input.id;
+      const num = Number(input.value);
+      const resultObj = calc(label, num);
 
       for (let input of inputsCollection) {
         let id = input.id;
@@ -24,43 +23,33 @@ function attachEventsListeners() {
       }
     }
 
-    function calc(type, num) {
-      return funcObj[type](num);
-    }
+    function calc(type, num) => funcObj[type](num)
 
     funcObj = {
-      days: (num) => {
-        return {
+      days: (num) => ({
           days: num,
           hours: num * 24,
           minutes: num * 1440,
           seconds: num * 86400,
-        };
-      },
-      hours: (num) => {
-        return {
+        }),
+      hours: (num) => ({
           days: num / 24,
           hours: num,
           minutes: (num / 24) * 1440,
           seconds: (num / 24) * 86400,
-        };
-      },
-      minutes: (num) => {
-        return {
+        }),
+      minutes: (num) => ({
           days: num / 1440,
           hours: num / 60,
           minutes: num,
           seconds: num * 60,
-        };
-      },
-      seconds: (num) => {
-        return {
+        }),
+      seconds: (num) => ({
           days: num / 60 / 1440,
           hours: num / 60 / 60,
           minutes: num / 60,
           seconds: num,
-        };
-      },
+        })
     };
   }
 }
