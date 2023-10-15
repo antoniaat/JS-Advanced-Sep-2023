c58e064337aa4da8c8d43e333c87ee1e13fe34c6
Yavor Petkov <YavorPetkov@students.softuni.bg>
2023-10-15 14:40:49 +0300

    Simpler solution with only one object to convert the units.

--- 05-dom-events/exercise/07-distanceConverter/distanceConverter.js
+++ 05-dom-events/exercise/07-distanceConverter/distanceConverter.js
@@ -5,33 +5,18 @@ function attachEventsListeners() {
     const outputUnits = document.getElementById('outputUnits');
     const output = document.getElementById('outputDistance');
 
-    const convertInputToMeters = {
-        km: x => x * 1000,
-        m: x => x,
-        cm: x => x * 0.01,
-        mm: x => x * 0.001,
-        mi: x => x * 1609.34,
-        yrd: x => x * 0.9144,
-        ft: x => x * 0.3048,
-        in: x => x * 0.0254
-    }
-
-    const convertMetersToOutputUnits = {
-        km: x => x * 0.001,
-        m: x => x,
-        cm: x => x * 100,
-        mm: x => x * 1000,
-        mi: x => x * 0.00062137,
-        yrd: x => x * 1.0936133,
-        ft: x => x * 3.28,
-        in: x => x * 39.3700787
-    }
-
-
     convertButton.addEventListener('click', () => {
-        const meters = convertInputToMeters[inputUnits.value](Number(input.value))
+        let units = {
+            km: 1000,
+            m: 1,
+            cm: 0.01,
+            mm: 0.001,
+            mi: 1609.34,
+            yrd: 0.9144,
+            ft: 0.3048,
+            in: 0.0254
+        };
 
-        const outputValue = convertMetersToOutputUnits[outputUnits.value](meters)
-        output.value = outputValue
+        output.value = input.value * units[inputUnits.value] / units[outputUnits.value]
     })
 }
